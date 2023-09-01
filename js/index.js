63f7a93e32e828d2803676414a6f403be711d01a
Somel <somelahmed88@gmail.com>
2023-09-01 16:58:49 +0600

    updated

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-const loadData = async() => {
+//! Load Categories 
+const loadData = async () => {
   const res = await fetch(
     "https://openapi.programming-hero.com/api/videos/categories"
   );
@@ -7,32 +8,24 @@ const loadData = async() => {
   categoryList(categories);
 };
 
+
+//! Getting category from categories 
 const categoryList = (categories) => {
   const categoryListContainer = document.getElementById(
     "categoryListContainer"
   );
-    
-  
-  
   categoryListContainer.innerHTML = `
         ${categories
           .map(
-            (cat) => 
-              `<button id="btnCategory" onclick="handleShowCard('${cat.category_id}')" class="hover:bg-red-600 bg-primary px-8 py-1 rounded-sm">${cat.category}</button>`).join("") 
-              
-
-        }
-    `  ;
-
-    
-     
-      
-  }
-
+            (cat) =>
+              `<button id="btnCategory" onclick="handleShowCard('${cat.category_id}')" class="hover:bg-red-600 bg-primary px-8 py-1 rounded-sm">${cat.category}</button>`
+          )
+          .join("")}
+    `;
+};
 
 
 //! Handle Show cards
-
 const handleShowCard = async (category_id) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/videos/category/${category_id}`
@@ -43,41 +36,40 @@ const handleShowCard = async (category_id) => {
   const sortByDateBtn = document.getElementById("sortByDateBtn");
   sortByDateBtn.addEventListener("click", () => {
     sortByDateBtn.classList.add("bg-red-400");
-    
 
     showCardDetails(cardDetails, true);
-  })
+  });
   showCardDetails(cardDetails, false);
   sortByDateBtn.classList.remove("bg-red-400");
-
-
-
-  
 };
 const warning = () => {
   console.log("No data found");
 };
+
+
 //! Show Card Details
 const showCardDetails = (cardDetails, sortByView) => {
-  
+  //? Sorting By Views 
   const sorted = (a, b) => {
     const view1 = parseFloat(a.others.views.slice(0, -1));
-    const view2 = parseFloat(b.others.views.slice(0, -1))
+    const view2 = parseFloat(b.others.views.slice(0, -1));
 
-    if(view1 < view2) return 1;
-    else if(view1 > view2) return -1;
+    if (view1 < view2) return 1;
+    else if (view1 > view2) return -1;
     else return 0;
-  }
+  };
+
   const cardsContainer = document.getElementById("cardsContainer");
 
   cardsContainer.textContent = "";
 
-  if(sortByView) {
+  if (sortByView) {
     cardDetails.sort(sorted);
   }
+  //? Checking if the datas are available 
   if (cardDetails.length == 0) {
     const div = document.createElement("div");
-    
+
     div.classList = "flex flex-col rjustify-cente gap-6 my-4 ";
     div.innerHTML = `
         <div>
@@ -214,31 +206,40 @@ const showCardDetails = (cardDetails, sortByView) => {
       
       `;
     cardsContainer.appendChild(div);
-  } 
+  }
 
-    
-    cardDetails.forEach((card) => {
-    //?   Calculating Hours and Minutes 
+  cardDetails.forEach((card) => {
+
+    //?   Calculating Hours and Minutes
     const secondsToHoursAndMinutes = (seconds) => {
-        const hours  = Math.floor(seconds / 3600);
-        const minutes = Math.floor((seconds%3600)/60);
+      const hours = Math.floor(seconds / 3600);
+      const minutes = Math.floor((seconds % 3600) / 60);
 
-        return {hours , minutes};
-    }
-    const {hours, minutes} = secondsToHoursAndMinutes(card.others.posted_date);
+      return { hours, minutes };
+    };
+    const { hours, minutes } = secondsToHoursAndMinutes(
+      card.others.posted_date
+    );
 
-
-      const div = document.createElement("div");
-      cardsContainer.classList =
-        "grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-8 gap-8";
-      div.innerHTML = `
+    const div = document.createElement("div");
+    cardsContainer.classList =
+      "grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-8 gap-8";
+    div.innerHTML = `
            
             <figure class="relative"><img class="w-[400px] h-[200px] lg:w-[300px] lg:h-[200px] rounded-md" src=${
               card.thumbnail
             } alt="Shoes" />
 
             <p class="absolute bottom-2 right-2 bg-[#171717] text-gray-50 px-3 rounded-lg">
-                ${hours>0 ?minutes==0?hours+`hrs `+"ago":`${hours+"hrs"} ${minutes}`+` min `+` ago`:minutes>0?minutes+"ago":""}
+                ${
+                  hours > 0
+                    ? minutes == 0
+                      ? hours + `hrs ` + "ago"
+                      : `${hours + "hrs"} ${minutes}` + ` min ` + ` ago`
+                    : minutes > 0
+                    ? minutes + "ago"
+                    : ""
+                }
              </p>
             </figure>
             
@@ -280,10 +281,9 @@ const showCardDetails = (cardDetails, sortByView) => {
             </div>
             `;
 
-      cardsContainer.appendChild(div);
-    });
-  
+    cardsContainer.appendChild(div);
+  });
 };
-loadData();
-handleShowCard(1000)
 
+loadData();
+handleShowCard(1000);
